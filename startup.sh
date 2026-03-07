9286dd685fbd236298c6e4b99f801f6b1ae2a15a
Jared Morris <jaredmorris@mac.lan>
2026-03-07 15:42:33 +1000

    Updated startup.sh to ensure dependencies installed

--- startup.sh
+++ startup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Ensure dependencies are installed in the runtime environment
+pip install -r /home/site/wwwroot/requirements.txt
+
 gunicorn app.main:app \
   --workers 2 \
   --worker-class uvicorn.workers.UvicornWorker \
